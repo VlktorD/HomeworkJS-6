1d01cd162cdd956a18372cb810dcd0fb771ce48e
VlktorD <viktordimovski92@gmail.com>
2025-01-13 16:25:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myTitle").textContent = "Welcome to the incredible world of JavaScript!";
+document.querySelector(".paragraph").textContent = "This exercise is not just easy, it's fun and exciting!";
+document.querySelector(".second").textContent = "You can change anything with JavaScript, even the world!";
+document.querySelector(".specialText").textContent = "Now you know how to select and modify elements with JavaScript!";
+document.getElementById("heading1").textContent = "Congratulations! You've successfully changed this heading!";
+document.getElementById("heading3").textContent = "And this too, using the power of JavaScript!";
